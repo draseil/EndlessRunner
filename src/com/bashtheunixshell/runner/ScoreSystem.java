deb417f61f2b8397688ad9598ea310306ccbe11c
LIESard <liesfromalizard@gmail.com>
2015-01-12 01:55:04 +0100

    Fully functional game at this point

---
+++ src/com/bashtheunixshell/runner/ScoreSystem.java
@@ -0,0 +1,13 @@
+package com.bashtheunixshell.runner;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ScoreSystem {
+
+    private Path dir = Paths.get("scores/");;
+
+    public void writeScore(int score) {}
+
+    public void readScores() {}
+}
