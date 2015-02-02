2c62b7ac9b564477b58d07ac1c10a4cdfd8b4dd8
LIESard <liesfromalizard@gmail.com>
2015-02-02 18:25:04 +0100

    Updated files

--- src/com/bashtheunixshell/runner/ScoreSystem.java
+++ src/com/bashtheunixshell/runner/ScoreSystem.java
@@ -1,11 +1,8 @@
 package com.bashtheunixshell.runner;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class ScoreSystem {
 
-    private Path dir = Paths.get("scores/");;
+    //private Path dir = Paths.get("scores/");;
 
     public void writeScore(int score) {}
 
